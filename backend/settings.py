094667d2d51411956e85e24bfb21adb537cc7bfe
Tushar Muley <tusharmule232@gmail.com>
2025-05-27 14:36:41 +0530

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -186,14 +186,17 @@ SIMPLE_JWT = {
 
 
 if ENVIRONMENT == 'production':
-    # AWS S3 Settings for production
+    # S3 Storage settings
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = 'your-region'  # e.g., 'us-east-1'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')  # change if your region is different
 
-    # Media storage
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+
+    # MEDIA settings to use S3
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 else:
